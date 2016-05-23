0810ece84d1588cc22b61acadc60b1be2b536cf0
beru26 <berkes.edina@gmail.com>
2016-05-23 15:05:19 +0200

    Passgen

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,20 @@
-def passwordgen():
-    return
+# def passwordgen():
+#     return
+#
+#
+# def main():
+#     return
+#
+#
+# if __name__ == '__main__':
+#     main()
 
+import string
+import random
 
-def main():
-    return
+def randompassword():
+    chars = string.unicode_uppercase + string.unicode_lowercase + string.digits
+    size = 4
+    return ''.join(random.choice(chars) for x in range(size,12))
 
-
-if __name__ == '__main__':
-    main()
+print(randompassword())
